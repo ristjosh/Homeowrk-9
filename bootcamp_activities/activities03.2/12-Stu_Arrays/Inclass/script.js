0714138968925ad67f39a6566ed8e5556f9eb675
Chris_remote <chris.woolcott@quotacy.com>
2021-06-23 21:44:46 -0500

    solutions and activities

---
+++ bootcamp_activities/activities03.2/12-Stu_Arrays/Inclass/script.js
@@ -0,0 +1,22 @@
+
+
+var students = ["Kabao", "Alena", "Jose", "Shawn", "Sam", "David"];
+
+console.log(students.length);
+
+console.log("Welcome to class " + students[0]);
+console.log("Welcome to class " + students[1]);
+console.log("Welcome to class " + students[2]);
+console.log("Welcome to class " + students[3]);
+console.log("Welcome to class " + students[4]);
+console.log("Welcome to class " + students[5]);
+
+
+students[0] = "Jack";
+
+if (students[0] === "Jack") {
+    console.log(students[0] + " is in class!");
+}
+
+
+
---
+++ bootcamp_activities/activities03.2/16-Stu_Functions/Inclass/script.js
@@ -0,0 +1,10 @@
+// Write Your Code Below
+var classStudents = ["Kabao", "Alena", "Jose", "Shawn", "Sam", "David"];
+
+console.log(classStudents.length);
+
+for (var i = 0; i < classStudents.length; i++) {
+    console.log("Great to see you, " + classStudents[i] + "!");
+}
+
+console.log(i);
