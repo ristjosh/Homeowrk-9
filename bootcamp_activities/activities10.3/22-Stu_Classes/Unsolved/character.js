99fba152688f830277c5d4dd4a40b67d020c7d5b
Chris Woolcott <chris.woolcott@quotacy.com>
2021-08-18 21:38:15 -0500

    solutions

--- bootcamp_activities/activities10.3/22-Stu_Classes/Unsolved/character.js
+++ bootcamp_activities/activities10.3/22-Stu_Classes/Unsolved/character.js
@@ -1,70 +1,35 @@
 class Character {
   // TODO: Add a constructor
-  constructor(name, strength, hitPoints) {
-    this.name = name;
-    this.strength = strength;
-    this.hitPoints = hitPoints;
-  }
-
   // TODO: Create a printStats() method that console logs `this.name`, `this.strength`, and `this.hitPoints`
-  printStats() {
-    console.log(`Stats for ${this.name} are:\n`)
-    console.log(`Each attack will do ${this.strength} damage`)
-    console.log(`${this.name} has ${this.hitPoints} hit points remaining`)
-  }
-
 
   // TODO: Create a isAlive() method that returns a boolean based on whether or not a character's "hitpoints" are <= 0
-  isAlive() {
-    if (this.hitPoints <= 0) {
-      console.log(`${this.name} is No Longer Living (KO)`);
-      return false;
-    }
-
-    return true;
-  }
 
   // TODO: Create a attack() method that accepts an opponent object and decreases the opponent's "hitPoints" by this character's strength
-  attack(opponent) {
-    console.log(`${this.name} attacks ${opponent.name}, for ${this.strength} damage.`)
-    opponent.hitPoints = opponent.hitPoints - this.strength;
-  }
-
 }
 
 // Creates two unique characters using the "character" constructor
 const grace = new Character('Grace', 30, 75);
 const dijkstra = new Character('Dijkstra', 20, 105);
-const bob = new Character('Bob', 30, 115);
 
-// const testObject = {
-//   "name": "TESTER",
-//   "hitPoints": 100
-// }
+// This keeps track of whose turn it is
+let graceTurn = true;
 
-// bob.attack(testObject);
+grace.printStats();
+dijkstra.printStats();
 
-// console.log("testObject", testObject)
+const turnInterval = setInterval(() => {
+  // If either character is not alive, end the game
+  if (!grace.isAlive() || !dijkstra.isAlive()) {
+    clearInterval(turnInterval);
+    console.log('Game over!');
+  } else if (graceTurn) {
+    grace.attack(dijkstra);
+    dijkstra.printStats();
+  } else {
+    dijkstra.attack(grace);
+    grace.printStats();
+  }
 
-// // This keeps track of whose turn it is
-// let graceTurn = true;
-
-// grace.printStats();
-// dijkstra.printStats();
-
-// const turnInterval = setInterval(() => {
-//   // If either character is not alive, end the game
-//   if (!grace.isAlive() || !dijkstra.isAlive()) {
-//     clearInterval(turnInterval);
-//     console.log('Game over!');
-//   } else if (graceTurn) {
-//     grace.attack(dijkstra);
-//     dijkstra.printStats();
-//   } else {
-//     dijkstra.attack(grace);
-//     grace.printStats();
-//   }
-
-//   // Switch turns
-//   graceTurn = !graceTurn;
-// }, 2000);
+  // Switch turns
+  graceTurn = !graceTurn;
+}, 2000);
