0ef6a280bac1ccfd94b3434cea1017e2279c4fc3
Chris Woolcott <chris.woolcott@quotacy.com>
2021-08-24 22:29:07 -0500

    express example

---
+++ bootcamp_activities/activities11.3/0-inclass_mall/data/storeData.js
@@ -0,0 +1,23 @@
+var storeArray = [
+    {
+        storeID: 1,
+        storeName: "Bread Pitt",
+        storeDescription: "Organic Wholesale Bakery",
+        status: "open"
+    },
+    {
+        storeID: 2,
+        storeName: "Thai Tanic",
+        storeDescription: "Thai Food To Go",
+        status: "closed"
+    }
+    ,
+    {
+        storeID: 3,
+        storeName: "Florist Gump",
+        storeDescription: "Florist and Home Decor",
+        status: "open"
+    }
+];
+
+module.exports = storeArray;
---
+++ bootcamp_activities/activities11.3/0-inclass_mall/server.js
@@ -0,0 +1,50 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
+var express = require("express");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port.
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//setup public folder for images, js, css
+app.use(express.static('public'));
+
+
+//ROUTES
+
+//API 
+require("./routes/apiRoutes")(app);
+
+// HTML 
+require("./routes/htmlRoutes")(app);
+
+
+
+
+
+
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
+
+
