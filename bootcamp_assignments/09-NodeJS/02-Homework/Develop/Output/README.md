fcc7fe97acda9d03af90cb0a6c8133a0c6a2807b
ristjosh <ristjosh1@icloud.com>
2021-08-31 13:30:46 -0500

    first commit

---
+++ bootcamp_assignments/09-NodeJS/02-Homework/Develop/Output/README.md
@@ -0,0 +1,22 @@
+# g
+
+  # g
+
+  # g
+
+  # g
+
+  # Apache
+
+  # g
+
+  # g
+
+  # g
+
+  # g
+
+  # g
+    Feel free to contact me here!
+  
+  # [![macOS](https://svgshare.com/i/ZjP.svg)](https://svgshare.com/i/ZjP.svg)
--- bootcamp_assignments/09-NodeJS/02-Homework/Develop/utils/generateMarkdown.js
+++ bootcamp_assignments/09-NodeJS/02-Homework/Develop/utils/generateMarkdown.js
@@ -1,6 +1,9 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return "[![macOS](https://svgshare.com/i/ZjP.svg)](https://svgshare.com/i/ZjP.svg)";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,8 +15,29 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  console.log(data)
+  return `# ${data.projectTitle}
 
+  # ${data.description}
+
+  # ${data.installation}
+
+  # ${data.usage}
+
+  # ${data.license}
+
+  # ${data.contributing}
+
+  # ${data.tests}
+
+  # ${data.questions}
+
+  # ${data.username}
+
+  # ${data.email}
+    Feel free to contact me here!
+  
+  # ${renderLicenseBadge()}
 `;
 }
 
